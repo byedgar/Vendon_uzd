f0b9d5703f0a838bd444375714f0a4348def6a87
Edgars <edvelg@gmail.com>
2019-06-19 15:46:56 +0300

    Small code change

--- assets/js/check.js
+++ assets/js/check.js
@@ -1,53 +1,51 @@
 function validatename() {
-	var error=0;
-	var x = document.forms["form1"]["firstname"].value;
-	var y = document.forms["form1"]["tests"].value;
+    var error = 0;
+    var x = document.forms["form1"]["firstname"].value;
+    var y = document.forms["form1"]["tests"].value;
     if (x == "") {
         text = "Vārds nav ievadīts";
-		alert(text);
-		error++;
+        alert(text);
+        error++;
         return false;
 
     }
-	if (x.length <=3) {
+    if (x.length <= 3) {
         text = "Vārds ir mazāk par 4 simboliem";
-		alert(text);
-		error++;
+        alert(text);
+        error++;
         return false;
-		
+
     }
-	if (y < 1) {
+    if (y < 1) {
         text = "Nav izvēlēta ankēta";
-		alert(text);
-		error++;
+        alert(text);
+        error++;
         return false;
-	
+
     }
 }
 
 
-function validateanswer(){
+function validateanswer() {
+    var x = document.getElementsByName('atbilde');
+    for (var i = 0, length = x.length; i < length; i++)
+    {
+        if (x[i].checked)
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	var x = document.getElementsByName('atbilde');
 
-for (var i = 0, length = x.length; i < length; i++)
-{
- if (x[i].checked)
- {
- return true; 
- }else{
-	return false; 
- }
-}
-	
-	
-	
-	
-	
-	
+
+
+
+
     if (x == "") {
         text = "Nav izvēlēta atbilde";
-		alert(text);
+        alert(text);
         return false;
     }
 }
